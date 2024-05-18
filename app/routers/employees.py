9192e349acc15da408a07c16d94761db825f0efb
Jay Anton Roblico <jayanton.roblico@gmail.com>
2024-05-18 22:10:54 +0800

    reacreate employee POST method API

--- app/routers/employees.py
+++ app/routers/employees.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel, EmailStr
 from app.database import get_db
-from typing import List, Annotated
+from typing import Annotated
 from sqlalchemy.orm import Session
-from enum import Enum
 
 from app.models import employee
+from app.models.employee import Employee
 from app.database import engine
+from app.dto.employee import CreateEmployeeDto
 
 employee.Base.metadata.create_all(bind=engine)
 
@@ -15,43 +15,25 @@ router = APIRouter()
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-class Status(str, Enum):
-    regular = 'regular'
-    contractual = 'contractual'
-
-class Benefits(str, Enum):
-    hmo = 'HMO'
-    sss = 'SSS'
-    pag_ibig = 'PAG IBIG'
-
-class Employee(BaseModel):
-    first_name: str
-    last_name: str
-    email: EmailStr
-    status: Status
-    number_of_leaves: int = 0
-    benefits: List[Benefits] = None
-    contract_end_date: str = None
-    project: str = None
 
 @router.post("/employees/", tags=["employees"])
-async def create_employee(employee: Employee, db: db_dependency):
-    # db_employee=Employee(
-    #     first_name=employee.first_name,
-    #     last_name=employee.last_name,
-    #     email=employee.email,
-    #     status=employee.status,
-    #     number_of_leaves=employee.number_of_leaves,
-    #     benefits=employee.benefits,
-    #     contract_end_date=employee.contract_end_date,
-    #     project=employee.project
-    # )
+async def create_employee(employee: CreateEmployeeDto, db: db_dependency):
+    db_employee=Employee(
+        first_name=employee.first_name,
+        last_name=employee.last_name,
+        email=employee.email,
+        status=employee.status,
+        number_of_leaves=employee.number_of_leaves,
+        benefits=employee.benefits,
+        contract_end_date=employee.contract_end_date,
+        project=employee.project
+    )
 
-    # print(db_employee)
+    print(db_employee)
 
-    # db.add(db_employee)
-    # db.commit()
-    # db.refresh(db_employee)
+    db.add(db_employee)
+    db.commit()
+    db.refresh(db_employee)
 
     return employee
 
